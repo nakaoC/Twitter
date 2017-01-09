c10369add638939038921fd3d2c74d207a29b29f
ryonakao <rodriguez.nak@gmail.com>
2017-01-09 11:21:00 +0900

    Make Like model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107083010) do
+ActiveRecord::Schema.define(version: 20170109022003) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "note_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "notes", force: :cascade do |t|
     t.text     "content"
