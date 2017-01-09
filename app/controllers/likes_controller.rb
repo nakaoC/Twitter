4eb5921c3bc9923dd451207f9c02634c1df45da7
ryonakao <rodriguez.nak@gmail.com>
2017-01-09 12:00:01 +0900

    Destoy Like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,16 @@
 class LikesController < ApplicationController
+
   def like
     note = Note.find(params[:note_id]) #送られたnote_idに対応したnoteインスタンスを生成
     like = current_user.likes.build(note_id: note.id) #現在ログイン中のユーザーに紐付いたlikeインスタンスを生成し、今見ていた投稿のidをnote_idカラムに入れる
     like.save
+    redirect_to note
+  end
+
+  def unlike
+    note = Note.find(params[:note_id])
+    like = Like.find_by(user_id: current_user.id, note_id: note.id)
+    like.destroy
     redirect_to note
   end
 end
